11b72c4c9785b0f8ad37cf709a82e659c51056ea
Mika Vilpas <mika.vilpas@gmail.com>
2016-08-26 11:38:32 +0300

    Add alternative solutions for "find last element in list"

--- src/main/scala/ListSolutions.scala
+++ src/main/scala/ListSolutions.scala
@@ -1,5 +1,22 @@
+import scala.annotation.tailrec
+
 class ListSolutions
 
 object ListSolutions {
   def last[A](l: List[A]) = l.last
+
+  def lastRecursive[A](l: List[A]) = {
+    @tailrec
+    def getLast(l: List[A]): A =
+      if (l.size == 1) l.head
+      else getLast(l.tail)
+
+    getLast(l)
+  }
+
+  @tailrec
+  def lastRecursiveIndexed[A](l: List[A], index: Long = 0): A = {
+    if (index == l.size) l.head
+    else lastRecursiveIndexed(l.tail, index + 1)
+  }
 }
--- src/test/scala/ListSolutionsTests.scala
+++ src/test/scala/ListSolutionsTests.scala
@@ -1,7 +1,7 @@
 import org.scalatest.WordSpec
 
 class ListSolutionsTests extends WordSpec {
-  "Processing a list" should {
+  "last element of list" should {
     "find last element" in {
       assert(ListSolutions.last(List(1,2)) == 2)
     }
@@ -10,6 +10,18 @@ class ListSolutionsTests extends WordSpec {
       intercept[NoSuchElementException] {
         ListSolutions.last(List())
       }
+    }
+  }
+
+  "last element of list recursively" should {
+    "find last element" in {
+      assert(ListSolutions.lastRecursive(List(1, 2)) == 2)
+    }
+  }
+
+  "last element of list recursively, indexed (stupid implementation but good for practice)" should {
+    "find last element" in {
+      assert(ListSolutions.lastRecursiveIndexed(List(1, 2)) == 2)
     }
   }
 }
