380e8d1e68285e2890a61aa2ec0e4ad4e39eab6c
Mika Vilpas <mika.vilpas@gmail.com>
2016-09-13 09:29:47 +0300

    Add timer for timing execution speed

--- src/main/scala/adventofcode/Utils.scala
+++ src/main/scala/adventofcode/Utils.scala
@@ -6,3 +6,14 @@ object InputReader {
     File(url.getPath()).slurp()
   }
 }
+
+object Timer {
+  // http://biercoff.com/easily-measuring-code-execution-time-in-scala
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) + "ns")
+    result
+  }
+}
